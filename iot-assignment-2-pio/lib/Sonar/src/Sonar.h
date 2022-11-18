7036500346d85070e5b7b978dac379ac1d16504d
PtrTella <tellarinipietro@gmail.com>
2022-11-18 12:48:26 +0100

    Create Sonar.h

---
+++ iot-assignment-2-pio/lib/Sonar/src/Sonar.h
@@ -0,0 +1,24 @@
+#ifndef __SONAR__
+#define __SONAR__
+
+/* supponendo di eseguire il test 
+   in un ambiente a 20 °C 
+   https://it.wikipedia.org/wiki/Velocità_del_suono
+*/ 
+
+class Sonar{
+
+
+    public:
+        Sonar(int trigPin, int echoPin);
+        float getDistance();
+
+    private:
+        int trigPin;
+        int echoPin;
+        const double vs = 331.45 + 0.62*20;
+
+
+};
+
+#endif
