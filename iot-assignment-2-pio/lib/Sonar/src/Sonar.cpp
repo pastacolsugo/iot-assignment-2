3eb53284856bbaba4209430d6a17926843311340
PtrTella <tellarinipietro@gmail.com>
2022-11-18 12:48:18 +0100

    Create Sonar.cpp

---
+++ iot-assignment-2-pio/lib/Sonar/src/Sonar.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include "Sonar.h"
+
+Sonar::Sonar(int trigPin, int echoPin){
+    this->trigPin = trigPin;
+    this->echoPin = echoPin;
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
+
+float Sonar::getDistance(){
+
+    /* invio impulso */
+    digitalWrite(this->trigPin,LOW);
+    delayMicroseconds(3);
+    digitalWrite(this->trigPin,HIGH);
+    delayMicroseconds(5);
+    digitalWrite(this->trigPin,LOW);
+    
+    /* ricevi l’eco */
+    long tUS = pulseInLong(this->echoPin, HIGH);
+    double t = tUS / 1000.0 / 1000.0 / 2;
+    double d = t*vs;
+
+    return d;
+}
