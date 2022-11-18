b2d40edb0e215247930b2b5604be0bb1cef9447d
PtrTella <tellarinipietro@gmail.com>
2022-11-18 12:51:02 +0100

    Create ValveTask.h

--- iot-assignment-2-pio/src/ValveTask.h
+++ iot-assignment-2-pio/src/ValveTask.h
@@ -2,9 +2,29 @@
 #define __VALVETASK__
 
 #include <Task.h>
-//#include
+#include <Timer.h>
+#include <Led.h>
+#include <Sonar.h>
+#include <Valve.h>
 
 class ValveTask: public Task {
+
+    long long time;
+    int ledRed_pin;
+    int ledGre_pin;
+    int sonar_trigPin;
+    int sonar_echoPin;
+    int valve_pin;
+    Led* redLed;
+    Led* greLed;
+    Sonar* sonar;
+    Valve* valve;
+
+    public:
+        ValveTask(int led, int grrenLed, int sonarTrigPin, int sonarEchoPin, int valvePin);
+        void init(int period);
+        void tick();
+
 };
 
 #endif
