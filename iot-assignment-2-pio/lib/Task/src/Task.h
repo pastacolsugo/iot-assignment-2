b10a6124664f0eb960bd1d9405a52eabdccf365b
PtrTella <tellarinipietro@gmail.com>
2022-11-21 20:10:03 +0100

    Update Task.h

--- iot-assignment-2-pio/lib/Task/src/Task.h
+++ iot-assignment-2-pio/lib/Task/src/Task.h
@@ -1,14 +1,26 @@
 #ifndef __TASK__
 #define __TASK__
 
+#include "Status.h"
+
 class Task {
   int myPeriod;
   int timeElapsed;
+
+protected:
+  Status* STATUS;
   
 public:
-  virtual void init(int period){
-    myPeriod = period;  
+
+
+  virtual void init(int period, Status* state){
+    myPeriod = period; 
     timeElapsed = 0;
+    STATUS = state;
+  }
+
+  virtual void setPeriod(int period){
+    myPeriod = period;
   }
 
   virtual void tick() = 0;
