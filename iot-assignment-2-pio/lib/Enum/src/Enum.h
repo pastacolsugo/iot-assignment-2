322a8561b72e638664f886e2357a6b722ab3af68
Ugo <ugobaroncini@gmail.com>
2022-11-22 16:14:01 +0100

    Refactored `Enum.h`

--- iot-assignment-2-pio/lib/Enum/src/Enum.h
+++ iot-assignment-2-pio/lib/Enum/src/Enum.h
@@ -1,10 +1,8 @@
 #ifndef __ENUM__
 #define __ENUM__
 
-
-        enum State {IDLE, PREALARM, ALARM};
-        enum Control {MANUAL, AUTO};
-        enum Light {ON, OFF};
-
+enum State {IDLE, PREALARM, ALARM};
+enum Control {MANUAL, AUTO};
+enum Light {ON, OFF};
 
 #endif
