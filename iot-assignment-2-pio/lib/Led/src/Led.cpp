94969ae854ae55ad28b11b8b9fd018c61845df52
Ugo <ugobaroncini@gmail.com>
2022-11-22 16:13:11 +0100

    Refactored `Led.cpp`

--- iot-assignment-2-pio/lib/Led/src/Led.cpp
+++ iot-assignment-2-pio/lib/Led/src/Led.cpp
@@ -5,15 +5,15 @@
 
 Led::Led(int pin){
   this->pin = pin;
-  pinMode(pin,OUTPUT);  
+  pinMode(pin, OUTPUT);  
 }
 
 void Led::switchOn(){
-  digitalWrite(pin,HIGH);
+  digitalWrite(pin, HIGH);
 }
 
 void Led::switchOff(){
-  digitalWrite(pin,LOW);
+  digitalWrite(pin, LOW);
 };
 
 void Led::blink(){
