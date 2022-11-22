7df14304b09572dba9d5bf7f32c1810f387cc2f8
Ugo <ugobaroncini@gmail.com>
2022-11-22 15:58:15 +0100

    Refactored `Valve.cpp`

--- iot-assignment-2-pio/lib/Valve/src/Valve.cpp
+++ iot-assignment-2-pio/lib/Valve/src/Valve.cpp
@@ -8,6 +8,10 @@ Valve::Valve(int pin){
 }
 
 void Valve::setPosition(int pos){
-    pos = (pos >= 180) ? 180 : ((pos <= 0) ? 0 : pos);
+    if (pos > 0) {
+        pos = 0;
+    } else if (pos > 180) {
+        pos = 180;
+    }
     valve.write(pos);
 }
