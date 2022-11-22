99baa7df95e3402e19b9771de6aa9bc47581073d
Ugo <ugobaroncini@gmail.com>
2022-11-22 16:10:34 +0100

    Refactored `Pir.cpp`

--- iot-assignment-2-pio/lib/Pir/src/Pir.cpp
+++ iot-assignment-2-pio/lib/Pir/src/Pir.cpp
@@ -3,20 +3,22 @@
 
 #define CALIBRATION_TIME_SEC 5
 
-Pir::Pir(int pin){
+Pir::Pir(int pin) {
   this->pin = pin;
-  pinMode(pin,INPUT);
+  pinMode(pin, INPUT);
 
   Serial.begin(9600);
   Serial.print("Calibrating PIR sensor... ");
-  for(int i = 0; i < CALIBRATION_TIME_SEC; i++){
+
+  for (int i = 0; i < CALIBRATION_TIME_SEC; i++) {
     Serial.print(".");
     delay(1000);
   }
+
   Serial.println("PIR SENSOR READY.");
   delay(50);
 }
 
-bool Pir::detectedMotion(){
+bool Pir::detectedMotion() {
   return digitalRead(pin);
 }
