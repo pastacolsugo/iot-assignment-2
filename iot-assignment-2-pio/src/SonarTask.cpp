1f43fa94fe399b3fd48a9531259bdb87e7f42246
Ugo <ugobaroncini@gmail.com>
2022-11-23 16:53:36 +0100

    Refactored `SonarTask.cpp` for readability and cleanliness.

--- iot-assignment-2-pio/src/SonarTask.cpp
+++ iot-assignment-2-pio/src/SonarTask.cpp
@@ -1,4 +1,3 @@
-
 #include <SonarTask.h>
 
 #define WL1 100
@@ -9,15 +8,13 @@
 #define PERIOD_TASK_PREALARM 30
 #define PERIOD_TASK_ALARM 10
 
-SonarTask::SonarTask(int trig, int echo, Status* state) {
-  this->echo_pin = echo;
-  this->trig_pin = trig;
+SonarTask::SonarTask(int trig_pin, int echo_pin, Status* state) {
+  this->echo_pin = echo_pin;
+  this->trig_pin = trig_pin;
   this->status = state;
 }
 
-void SonarTask::init(){
-  SonarTask::init(PERIOD_TASK_NORMAL);
-}
+void SonarTask::init() { SonarTask::init(PERIOD_TASK_NORMAL); }
 
 void SonarTask::init(int period) {
   Task::init(period);
@@ -25,18 +22,22 @@ void SonarTask::init(int period) {
 }
 
 void SonarTask::tick() {
-  int tmp = sonar->getDistance();
-  tmp = (WLMAX < tmp) ? WLMAX : WLMAX - tmp;
-  status->setWater(tmp);
+  int sonarReading = sonar->getDistance();
+  int waterLevel = (WLMAX < sonarReading) ? WLMAX : WLMAX - sonarReading;
+  status->setWater(waterLevel);
 
-  if (tmp > WL2) {
+  if (waterLevel > WL2) {
     status->setState(State::ALARM);
     setPeriod(PERIOD_TASK_ALARM);
-  } else if (tmp > WL1) {
+    return;
+  }
+
+  if (waterLevel > WL1) {
     status->setState(State::PREALARM);
     setPeriod(PERIOD_TASK_PREALARM);
-  } else {
-    status->setState(State::NORMAL);
-    setPeriod(PERIOD_TASK_NORMAL);
+    return;
   }
+
+  status->setState(State::NORMAL);
+  setPeriod(PERIOD_TASK_NORMAL);
 }
