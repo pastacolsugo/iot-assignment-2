2d4580a8c5f4ab7a8bdb2b3ec35a4cdb4c67799a
Ugo <ugobaroncini@gmail.com>
2022-11-23 17:18:17 +0100

    Used `toggle()` method of `Led` and refactored for readability.

--- iot-assignment-2-pio/include/StoplightTask.h
+++ iot-assignment-2-pio/include/StoplightTask.h
@@ -1,28 +1,23 @@
 #ifndef __STOPLIGHTTASK__
 #define __STOPLIGHTTASK__
 
-#include <Task.h>
-#include <Status.h>
 #include <Led.h>
+#include <Status.h>
+#include <Task.h>
 
-class StoplightTask: public Task{
+class StoplightTask : public Task {
+ private:
+  int redled_pin;
+  int greenled_pin;
+  Led* greenLed;
+  Led* redLed;
+  Status* status;
 
-    private:
-        int redled_pin;
-        int greenled_pin;
-        bool ledOn;
-        Led* greenLed;
-        Led* redLed;
-        Status* status;
-
-    public:
-        StoplightTask(int red_pin, int green_pin, Status* status);
-        void init(int period);
-        void init();
-        void run();
-
-
+ public:
+  StoplightTask(int red_pin, int green_pin, Status* status);
+  void init(int period);
+  void init();
+  void run();
 };
-
 
 #endif
--- iot-assignment-2-pio/src/StoplightTask.cpp
+++ iot-assignment-2-pio/src/StoplightTask.cpp
@@ -1,51 +1,36 @@
-
 #include "StoplightTask.h"
 
 #define PERIOD_TASK 1000
 
-StoplightTask::StoplightTask(int red_pin, int green_pin, Status* status){
-    this->redled_pin = red_pin;
-    this->greenled_pin = green_pin;
-    this->ledOn = false;
-    this->status = status;
+StoplightTask::StoplightTask(int red_pin, int green_pin, Status* status) {
+  this->redled_pin = red_pin;
+  this->greenled_pin = green_pin;
+  this->status = status;
 }
 
-void StoplightTask::init(){
-    StoplightTask::init(PERIOD_TASK);
+void StoplightTask::init() { StoplightTask::init(PERIOD_TASK); }
+
+void StoplightTask::init(int period) {
+  Task::init(period);
+  this->greenLed = new Led(greenled_pin);
+  this->redLed = new Led(this->redled_pin);
 }
 
-void StoplightTask::init(int period){
-    Task::init(period);
-    this->greenLed = new Led(greenled_pin);
-    this->redLed = new Led(this->redled_pin);
-}
-
-void StoplightTask::run(){
-    
-    switch (status->getState()){
-
+void StoplightTask::run() {
+  switch (status->getState()) {
     case State::PREALARM:
-        greenLed->switchOn();
-        if(ledOn){
-            redLed->switchOff();
-            ledOn = false;
-        }else {
-            redLed->switchOn();
-            ledOn = true;
-        }
-        break;
+      greenLed->switchOn();
+      redLed->toggle();
+      return;
 
     case State::ALARM:
-        greenLed->switchOff();
-        redLed->switchOn();
-        ledOn = true;
-        break;
+      greenLed->switchOff();
+      redLed->switchOn();
+      return;
 
     default:
-        greenLed->switchOn();
-        redLed->switchOff();
-        ledOn = false;
-        break;
-    }
-
+      greenLed->switchOn();
+      redLed->switchOff();
+      return;
+  }
 }
