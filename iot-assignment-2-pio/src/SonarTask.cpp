61e42bd48916673271441cf77d1ac8076c3bf703
Pietro <tellarinipietro@gmail.com>
2022-11-23 12:39:54 +0100

    Update SonarTask.cpp

--- iot-assignment-2-pio/src/SonarTask.cpp
+++ iot-assignment-2-pio/src/SonarTask.cpp
@@ -5,10 +5,18 @@
 #define WL2 300
 #define WLMAX 1000
 
+#define PERIOD_TASK_NORMAL 50
+#define PERIOD_TASK_PREALARM 30
+#define PERIOD_TASK_ALARM 10
+
 SonarTask::SonarTask(int trig, int echo, Status* state) {
   this->echo_pin = echo;
   this->trig_pin = trig;
   this->status = state;
+}
+
+void SonarTask::init(){
+  SonarTask::init(PERIOD_TASK_NORMAL);
 }
 
 void SonarTask::init(int period) {
@@ -21,14 +29,14 @@ void SonarTask::tick() {
   tmp = (WLMAX < tmp) ? WLMAX : WLMAX - tmp;
   status->setWater(tmp);
 
-  // Controllo forse errato...lo stato dovrebeb cambiare anche se sono in
-  // manuale. if(status->matchValveStatus(Control::AUTO)){}
-
   if (tmp > WL2) {
     status->setState(State::ALARM);
+    setPeriod(PERIOD_TASK_ALARM);
   } else if (tmp > WL1) {
     status->setState(State::PREALARM);
+    setPeriod(PERIOD_TASK_PREALARM);
   } else {
     status->setState(State::NORMAL);
+    setPeriod(PERIOD_TASK_NORMAL);
   }
 }
