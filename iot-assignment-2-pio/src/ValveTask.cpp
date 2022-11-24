54adcf55007579b23a0d67e86aab25a5ad27387e
Ugo <ugobaroncini@gmail.com>
2022-11-24 17:31:07 +0100

    Fixed renamed parameters in `ValveTask.cpp`

--- iot-assignment-2-pio/src/ValveTask.cpp
+++ iot-assignment-2-pio/src/ValveTask.cpp
@@ -1,5 +1,7 @@
 #include "ValveTask.h"
 
+#include <Parameters.h>
+
 ValveTask::ValveTask(int valve_pin, Status* state) {
   this->valve = new Valve(valve_pin);
   this->status = state;
@@ -11,12 +13,13 @@ void ValveTask::run() {
       valve->setPosition(status->getManualValvePosition());
       return;
     }
-    
-    int position = map(status->getWater(), WL2, BH, 0, 180);
+
+    int position =
+        map(status->getWater(), WATER_LEVEL_2, WATER_LEVEL_MAX, 0, 180);
     valve->setPosition(position);
     status->setValvePosition(position);
     return;
-  } 
+  }
 
   status->setAutoValvePosition(0);
   valve->setPosition(0);
