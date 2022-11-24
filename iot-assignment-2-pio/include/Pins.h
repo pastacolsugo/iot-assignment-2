02d54250030960fbd784dbe4d06116872710c03f
LucaBighini <bighini.luca@gmail.com>
2022-11-24 11:55:26 +0100

    Update Pins.h

--- iot-assignment-2-pio/include/Pins.h
+++ iot-assignment-2-pio/include/Pins.h
@@ -1,10 +1,24 @@
 #ifndef __PINS__
 #define __PINS__
 
+#define LED_LAMP 13
+#define PHOTORESISTOR A0
+#define PIR 2
+
+#define SONAR_TRIG 8
+#define SONAR_ECHO 7
+
+#define LED_GREEN 12
+#define LED_RED 11
+
 // Used into LcdTask.h
 /* Wiring: SDA => A4, SCL => A5 */
 #define LCD_SDA A4
 #define LCD_SCL A5
 
+#define BUTTON 10
+#define POTENZIOMETER A1
+
+#define SERVO 9
 
 #endif
